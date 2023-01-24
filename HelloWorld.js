1e01ec6d3a256dcdb0b57d6ee42c840a76ec86ba
Djob7 <bayoudhyoussef7@gmail.com>
2023-01-24 11:40:39 +0100

    first commit

---
+++ HelloWorld.js
@@ -0,0 +1,13 @@
+// const fs = require('fs')
+// const bo = fs.readFileSync('./HelloWorld.txt','utf-8')
+
+// // console.log("Finish")
+
+// const http = require('http')
+
+// http.createServer((req,res)=>{
+//     res.write(bo)
+//     res.end()
+// }).listen(3000, console.log('Server is running'))
+
+console.log("Hello world")
